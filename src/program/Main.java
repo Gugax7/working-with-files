4b7fdd7ff299fa4391ae95c315301103a31d13dc
Gustavo Salmazo <gux.salmazo@gmail.com>
2023-01-24 15:30:35 -0300

    reading .txt with buffered and file reader

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,31 +1,29 @@
 package program;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		print("Enter folder path:");
-		String strPath = sc.nextLine();
-		File path = new File(strPath);
-		File[] folders = path.listFiles(File::isDirectory);
-		print("Folders: ");
-		for(File x:folders) {
-			System.out.println(x);
+		System.out.println("Path: ");
+		String path = sc.nextLine();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path)))
+		{
+			String line = br.readLine();
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
 		}
-		File[] files = path.listFiles(File::isFile);
-		print("Files: ");
-		for(File f: files) {
-			System.out.println(f);
-		}
-		boolean success = new File(strPath + "\\subdir").mkdir();
-		System.out.println(success);
-	}
-	
-	public static void print(String string) {
-		System.out.println(string);
-	}
+		
 
+
+	}
 }
